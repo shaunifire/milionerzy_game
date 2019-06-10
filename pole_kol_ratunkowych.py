d12ae65f60f8521224389cd3265dfe6b0b922c8f
giomio <50331971+giomio@users.noreply.github.com>
2019-06-11 01:48:42 +0200

    Update pole_kol_ratunkowych.py
    Dodałem i ułożyłem symetrycznie obrazki kół ratunkowych oraz ich znikanie po użyciu, kliknięcie przycisku przekazuje zmiennej wybor_kola odpowiednią literkę aktywującą wybrane koło ratunkowe. Nie wiem co z tłem 1. nie umiem go wstawić :D 2. nie wiem jakie kolory wybrała reszta. Proszę o uwagi oraz co mogę teraz dorobić.

--- pole_kol_ratunkowych.py
+++ pole_kol_ratunkowych.py
@@ -2,29 +2,33 @@ from tkinter import *
 from PIL import Image, ImageTk
 okno_ratunkowe=Tk()
 okno_ratunkowe.title("Koła ratunkowe")
-okno_ratunkowe.geometry("400x150")
-telefon = Image.open("C:\\Users\\Mikolaj\\Desktop\\telefon.png")
+okno_ratunkowe.geometry("457x204")
+
+telefon = Image.open("C:\\Users\\Mikolaj\\Desktop\\programowanie\\tel.png")
 telefonTk=ImageTk.PhotoImage(telefon)
+fifty_fifty = Image.open("C:\\Users\\Mikolaj\\Desktop\\programowanie\\502.png")
+fifty_fiftyTk=ImageTk.PhotoImage(fifty_fifty)
+publicznosc = Image.open("C:\\Users\\Mikolaj\\Desktop\\programowanie\\pub.png")
+publicznoscTk=ImageTk.PhotoImage(publicznosc)
 
 def wybor_kola_telefon():
     wybor_kola="T"
     print(wybor_kola)
-    przyciskPoprzednie=publicznosc
-    przyciskPoprzednie.destroy()
+    telefon.destroy()
 def wybor_kola_publicznosc():
         wybor_kola="P"
         print(wybor_kola)
-        wybor_kola_publicznosc.destroy()
+        publicznosc.destroy()
 def wybor_kola_fifty_fifty():
     wybor_kola="F"
     print(wybor_kola)
-    wybor_kola_fifty_fifty.destroy()
+    fifty_fifty.destroy()
 
 
-publicznosc=Button(okno_ratunkowe, text = "Pomoc Publiczności",command = wybor_kola_telefon)
-fifty_fifty=Button(okno_ratunkowe, text = "Pół na pół",command=wybor_kola_publicznosc)
-telefon=Button(okno_ratunkowe,image=telefonTk,command=wybor_kola_fifty_fifty)
-publicznosc.place(x=50,y=50)
-fifty_fifty.place(x=180,y=50)
-telefon.place(x=250,y=50)
+publicznosc=Button(okno_ratunkowe, image=publicznoscTk,command = wybor_kola_publicznosc)
+fifty_fifty=Button(okno_ratunkowe, image=fifty_fiftyTk,command=wybor_kola_fifty_fifty)
+telefon=Button(okno_ratunkowe,image=telefonTk,command=wybor_kola_telefon)
+publicznosc.place(x=0,y=68)
+fifty_fifty.place(x=161,y=68)
+telefon.place(x=344,y=68)
 okno_ratunkowe.mainloop()
